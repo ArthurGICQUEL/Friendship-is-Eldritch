45adceb2602a1b3a48ce50d4b73c25e7952e9798
ArthurGICQUEL <arthurgcl@orange.fr>
2021-03-16 11:37:49 +0100

    Spells01

--- Assets/Scripts/Spells/Possession.cs
+++ Assets/Scripts/Spells/Possession.cs
@@ -2,6 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Possession : Spell {
-
+public class Possession : Spell
+{
+    public Room target;
+    public float stunTime = 3f;
+    public override void Cast()
+    {
+        AudioManager.Instance.Play("Possesion");
+        List<Human> humans = target.humans;
+        Human possessed = humans[Random.Range(0, humans.Count)];
+        possessed.Stun(stunTime);
+        humans.Remove(possessed);
+        for (int i = 0; i < humans.Count; i++)
+        {
+            humans[i].MindState = MindState.Panicking;
+        }
+    }
 }
--- Assets/Scripts/Spells/Summon.cs
+++ Assets/Scripts/Spells/Summon.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Summon : Spell {
+public class Summon : Spell
+{
+    public Room target;
+    public override void Cast()
+    {
+        AudioManager.Instance.Play("Summon");
 
+        GameObject.Instantiate<Minion>(Resources.Load(""), target.transform.position, Quaternion.identity);
+    }
 }
