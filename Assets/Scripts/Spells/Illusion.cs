92275be96345a370003b4451c4845b146feccbb6
ArthurGICQUEL <arthurgcl@orange.fr>
2021-03-16 15:57:43 +0100

    UI01 and Spells02

--- Assets/Scripts/Spells/Illusion.cs
+++ Assets/Scripts/Spells/Illusion.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Illusion : Spell
 {
-    public Room target;
-    public float power;
-    public override void Cast()
+    public float power = 1f;
+    public override bool Cast()
     {
-        AudioManager.Instance.Play("Illusion");
+        if (target.humans.Count == 0) return false;
+        //AudioManager.Instance.Play("Illusion");
         int nbHumans = target.humans.Count;
         for (int i = 0; i < nbHumans; i++)
         {
             target.humans[i].Sanity += power / nbHumans;
         }
+        return true;
     }
 }
